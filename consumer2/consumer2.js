806832b84b070cbec815617d1f90aee141d87ee0
Piyush Giri <piyushgiri630@gmail.com>
2025-12-05 11:23:24 +0530

    optimization

--- consumer2/consumer2.js
+++ consumer2/consumer2.js
@@ -1,7 +1,7 @@
 import amqp from 'amqplib';
 import dotenv from 'dotenv';
 dotenv.config();
-
+n 
 const QUEUE = 'task_queue_2';
 
 const RABBIT_URL = `amqp://${process.env.USERNAME}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.PORT}`;
@@ -12,6 +12,7 @@ export async function startConsumer2() {
 
     const conn = await amqp.connect(RABBIT_URL);
     const channel = await conn.createChannel();
+    channel.prefetch(1); // only process one message at a time
 
     await channel.assertQueue(QUEUE, { durable: true });
 
@@ -25,11 +26,13 @@ export async function startConsumer2() {
           try { payload = JSON.parse(payload); } catch (_) {}
 
           console.log(`[Consumer2] Received:`, payload);
-
-          channel.ack(msg);
+          setTimeout(() => {
+            channel.ack(msg);
+            console.log("[Consumer2] Message processed and acknowledged");
+          }, 2000);
         }
       },
-      { noAck: false }
+      { noAck: false } // by default it is false, means user has to manually acknowledge the message
     );
 
   } catch (error) {
